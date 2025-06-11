3e13cc2b93db117642fc08a02752c7f306995e0d
Jarno Orusalu <144801160+JARNO-IT23@users.noreply.github.com>
2025-06-11 15:08:28 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<?php
+<?php
 $host = '127.0.0.1';          
 $db   = 'helpdesk_system';             
 $user = 'admin';              
@@ -12,19 +11,4 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
     PDO::ATTR_EMULATE_PREPARES   => false,                 
-];
-=======
-<?php
-$host = 'localhost';
-$dbname = 'helpdesk_system';
-$username = 'admin';
-$password = 'passw0rd';
->>>>>>> 8a26b7ba9b55f479caca15a5bc5b2bcc7317b469
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    echo "Could not connect to the database: " . $e->getMessage();
-    exit;
-}
 ?>

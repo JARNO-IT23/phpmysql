e74a19f787afa527b91ba3a2cd29c138696ade31
Jarno Orusalu <144801160+JARNO-IT23@users.noreply.github.com>
2025-06-11 15:06:02 +0300

    fail

--- config.php
+++ config.php
@@ -1,13 +1,22 @@
-<?php
-$host = 'localhost';
-$dbname = 'helpdesk_system';
-$username = 'root';
-$password = '';
+<<?php
+$host = '127.0.0.1';          
+$db   = 'helpdesk_system';             
+$user = 'admin';              
+$pass = 'Passw0rd';          
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+    PDO::ATTR_EMULATE_PREPARES   => false,                 
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
-    die("Could not connect to the database: " . $e->getMessage());
+    echo "Could not connect to the database: " . $e->getMessage();
+    exit;
 }
 ?>
